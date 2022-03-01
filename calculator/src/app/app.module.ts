095344c24b4b714bac9e535004ee92ea44653968
David-Rueda <crueda@fastspring.com>
2022-02-28 20:25:41 -0500

    first commit

---
+++ calculator/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HeaderComponent } from "./components/header/header.component"
+
+@NgModule({
+  declarations: [
+    AppComponent, 
+    HeaderComponent],
+  entryComponents: [],
+  imports: [
+    BrowserModule, 
+    IonicModule.forRoot(), 
+    AppRoutingModule
+  ],
+  providers: [
+    { 
+      provide: RouteReuseStrategy, 
+      useClass: IonicRouteStrategy 
+    }],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
